3c7fe4c36b66ccbdecb3bcfe7af92979e9cbcc99
User Name <landonisaiah@gmail.com>
2025-08-04 02:49:38 -0700

    initial state defined

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 
 // STEP 1. create component
+// STEP 2. Define initial state
+//  2a. Create state variable `books` to store array of book objects
+//  2b. Create state variable `newBook` to handle inputs for new book additions.
 
 
-const Bookshelf = () => {                  // STEP 1. created component called `Bookshelf` with exports and imports
+const Bookshelf = () => {                      // STEP 1. created component called `Bookshelf` with exports and imports
+    const [books, setBooks] = useState([]);    // STEP 2a. state variable `books` holds empty array
+    conast[newBook, setNewBook] = useState({   // STEP 2b. state variable for new book additions, an object with title and author keys set to empty strings
+        title: '',
+        author: '',
+    });
+
 
     return (
         <>
